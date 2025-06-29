10a613c68e1cd364c848fdce6811068cf6f7dcb5
Bonnie <Bonnie>
2025-06-29 20:52:51 +0300

    adds code

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const userIdDisplay = document.getElementById("id");
+      if (userIdDisplay) {
+        userIdDisplay.textContent = data.id;
+      }
+    })
+    .catch((error) => {
+      const errorDisplay = document.getElementById("error");
+      if (errorDisplay) {
+        errorDisplay.textContent = error.message;
+      }
+    });
+}
